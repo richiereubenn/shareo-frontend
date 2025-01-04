1ae8777be938c3f43aa5219c87c36607c59fb49c
febeFlo <steffanyflorence@gmail.com>
2025-01-04 19:41:11 +0700

    Resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import AuthPages from './pages/AuthPage';
 import PaymentPage from './pages/PaymentPage';
 import TopUpPage from './pages/TopUpPage';
@@ -8,9 +8,10 @@ import InvitationCode from "./pages/InvitationCodePage";
 import JoinRoom from "./pages/JoinRoomPage";
 import QRScanner from "./pages/QRScannerPage";
 import Homepage from "./pages/Homepage";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ScanReceipt from './pages/scanReceipt.jsx';
+import ReceiptList from './pages/receiptList.jsx';
+import SelectItem from './pages/selectItem.jsx';
+import PaymentRecap from './pages/paymentRecap.jsx';
 
 function App() {
   const [transactionData, setTransactionData] = useState(null);
@@ -20,35 +21,24 @@ function App() {
       setTransactionData(data);
     }
   }
-  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/auth" replace />} />
-        <Route path="/auth" element={<AuthPages />} />
-        <Route path="/payment" element={
-          <PaymentPage 
-            onNavigate={handleNavigation}
-          />
-        } />
-        <Route path="/topup" element={
-          <TopUpPage 
-            onNavigate={handleNavigation}
-          />
-        } />
-        <Route path="/success" element={
-          <SuccessPage 
-            {...transactionData}
-          />
-        } />
-
-    <Route path="" element={<Homepage />} />
-    <Route path="/invitation-code" element={<InvitationCode />} />
-    <Route path="/join-room" element={<JoinRoom />} />
-    <Route path="/scan-qr" element={<QRScanner />} />
+    <Routes>
+      <Route path="/" element={<Navigate to="/auth" replace />} />
+      <Route path="/auth" element={<AuthPages />} />
+      <Route path="/payment" element={<PaymentPage onNavigate={handleNavigation} />} />
+      <Route path="/topup" element={<TopUpPage onNavigate={handleNavigation} />} />
+      <Route path="/success" element={<SuccessPage {...transactionData} />} />
+      <Route path="scanReceipt" element={<ScanReceipt />} />
+      <Route path="receiptList" element={<ReceiptList />} />
+      <Route path="selectItem" element={<SelectItem />} />
+      <Route path="paymentRecap" element={<PaymentRecap />} />
+      <Route path="/invitation-code" element={<InvitationCode />} />
+      <Route path="/join-room" element={<JoinRoom />} />
+      <Route path="/scan-qr" element={<QRScanner />} />
+      <Route path="" element={<Homepage />} />
     </Routes>
-    </BrowserRouter>
-  )
-};
+  );
+}
 
-export default App
+export default App;
