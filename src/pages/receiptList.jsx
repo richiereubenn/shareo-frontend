5e130c22bbffc6e3414411bfde88fc960ebae7d5
febeFlo <steffanyflorence@gmail.com>
2025-01-13 11:07:52 +0700

    Update route and receiptList

--- src/pages/receiptList.jsx
+++ src/pages/receiptList.jsx
@@ -94,145 +94,75 @@ const ReceiptList = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        navigate('/select-item', { state: { items, receiptName, scanDate, totals } });
+        navigate(`/select-item/${createRoom.roomCode}`, { state: { items, receiptName, scanDate, totals } });
     };
 
     return (
         <div className="p-8 bg-white">
             {/* Real */}
-            {/* <h2 className="text-2xl font-bold mb-2 text-left">{receiptName}</h2> */}
-            {/* <p className="text-left text-gray-600 mb-8">{scanDate}</p> */}
-            {/* Dummy */}
-            <h2 className="text-2xl font-bold mb-2 text-left">{roomData?.room_name}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-left">{receiptName}</h2>
             <p className="text-left text-gray-600 mb-8">{scanDate}</p>
-            {/* Real */}
-            {/* <form onSubmit={handleSubmit}> */}
-            {/* <div className="grid grid-cols-4 gap-1 mb-4"> */}
-            {/* <span className="font-semibold">Nama</span> */}
-            {/* <span className="font-semibold">Qty</span> */}
-            {/* <span className="font-semibold">Satuan</span> */}
-            {/* <span className="font-semibold">Harga Total</span> */}
-            {/* </div> */}
-            {/* {items.map((item, index) => ( */}
-            {/* <div key={index} className="grid grid-cols-4 gap-2 mb-2"> */}
-            {/* <input */}
-            {/* type="text" */}
-            {/* value={item.name} */}
-            {/* disabled={!isEditing} */}
-            {/* onChange={(e) => handleInputChange(index, 'name', e.target.value)} */}
-            {/* className="self-center" */}
-            {/* /> */}
-            {/* <input */}
-            {/* type="number" */}
-            {/* value={item.quantity} */}
-            {/* disabled={!isEditing} */}
-            {/* onChange={(e) => handleInputChange(index, 'quantity', e.target.value)} */}
-            {/* className="self-center" */}
-            {/* /> */}
-            {/* <input */}
-            {/* type="text" */}
-            {/* value={item.price} */}
-            {/* disabled={!isEditing} */}
-            {/* onChange={(e) => handleInputChange(index, 'price', e.target.value)} */}
-            {/* className="self-center" */}
-            {/* /> */}
-            {/* <input */}
-            {/* type="text" */}
-            {/* value={(parseFloat(item.price) * parseInt(item.quantity)).toFixed(2)} */}
-            {/* disabled */}
-            {/* className="self-center" */}
-            {/* /> */}
-            {/* </div> */}
-            {/* ))} */}
-            {/* <div className="flex justify-between mt-8">  */}
-            {/* <div className="text-left">  */}
-            {/* <p className="text-lg">Jumlah</p>  */}
-            {/* <p className="text-lg">Tax</p>  */}
-            {/* <p className="text-lg font-bold text-indigo-700">Jumlah Total</p>  */}
-            {/* </div>  */}
-            {/* <div className="text-right">   */}
-            {/* <p className="text-lg">Rp {totals.subtotal.toFixed(2)}</p>  */}
-            {/* <p className="text-lg">Rp {totals.tax.toFixed(2)}</p>  */}
-            {/* <p className="text-lg font-bold text-indigo-700">Rp {totals.total.toFixed(2)}</p>  */}
-            {/* </div>  */}
-            {/* </div> */}
-            {/* <div className="flex justify-between items-center mt-4"> */}
-            {/* <button */}
-            {/* type="button" */}
-            {/* onClick={() => navigate(-1)} */}
-            {/* className="py-2 px-4 me-2 w-1/2 bg-black text-white rounded-full hover:bg-gray-700 transition duration-200" */}
-            {/* > */}
-            {/* Edit */}
-            {/* </button> */}
-            {/* <button */}
-            {/* type="submit" */}
-            {/* className="py-2 px-4 ms-2 w-1/2 bg-indigo-700 text-white rounded-full hover:bg-indigo-800 transition duration-200" */}
-            {/* > */}
-            {/* Konfirmasi */}
-            {/* </button> */}
-            {/* </div> */}
-            {/* </form> */}
             {/* Dummy */}
+            {/* <h2 className="text-2xl font-bold mb-2 text-left">{roomData?.room_name}</h2>
+            <p className="text-left text-gray-600 mb-8">{scanDate}</p> */}
+            {/* Real */}
             <form onSubmit={handleSubmit}>
-                <div className="grid grid-cols-4 gap-1 mb-4" style={{ gridTemplateColumns: '3fr 1fr 2fr 2fr' }}>
+                <div className="grid grid-cols-4 gap-1 mb-4">
                     <span className="font-semibold">Nama</span>
                     <span className="font-semibold">Qty</span>
                     <span className="font-semibold">Satuan</span>
                     <span className="font-semibold">Harga Total</span>
                 </div>
-                <div>
-                    {items.map((item, index) => (
-                        <div key={index} className="grid grid-cols-4 gap-1 mb-2" style={{ gridTemplateColumns: '3fr 1fr 2fr 2fr' }}>
-                            <input
-                                type="text"
-                                value={item.item_name}
-                                onChange={(e) => handleInputChange(e, index, 'name')}
-                                className="self-center"
-                                disabled={!isEditing}
-                            />
-                            <input
-                                type="number"
-                                value={item.item_qty}
-                                onChange={(e) => handleInputChange(e, index, 'quantity')}
-                                className="self-center"
-                                disabled={!isEditing}
-                            />
-                            <input
-                                type="text"
-                                value={item.item_price}
-                                onChange={(e) => handleInputChange(e, index, 'price')}
-                                className="self-center"
-                                disabled={!isEditing}
-                            />
-                            <input
-                                type="text"
-                                value={(item.item_price * item.item_qty)}
-                                disabled
-                                className="self-center"
-                            />
-                        </div>
-                    ))}
-                </div>
-                <hr className="my-4 border-gray-400" />
-                <div className="flex justify-between mt-6">
+                {items.map((item, index) => (
+                    <div key={index} className="grid grid-cols-4 gap-2 mb-2">
+                        <input
+                            type="text"
+                            value={item.name}
+                            disabled={!isEditing}
+                            onChange={(e) => handleInputChange(index, 'name', e.target.value)}
+                            className="self-center"
+                        />
+                        <input
+                            type="number"
+                            value={item.quantity}
+                            disabled={!isEditing}
+                            onChange={(e) => handleInputChange(index, 'quantity', e.target.value)}
+                            className="self-center"
+                        />
+                        <input
+                            type="text"
+                            value={item.price}
+                            disabled={!isEditing}
+                            onChange={(e) => handleInputChange(index, 'price', e.target.value)}
+                            className="self-center"
+                        />
+                        <input
+                            type="text"
+                            value={(parseFloat(item.price) * parseInt(item.quantity)).toFixed(2)}
+                            disabled
+                            className="self-center"
+                        />
+                    </div>
+                ))}
+                <div className="flex justify-between mt-8">
                     <div className="text-left">
                         <p className="text-lg">Jumlah</p>
-                        {/* <p className="text-lg">Tax</p> */}
+                        <p className="text-lg">Tax</p>
                         <p className="text-lg font-bold text-indigo-700">Jumlah Total</p>
                     </div>
                     <div className="text-right">
-                        <p className="text-lg">Rp 227.75</p>
-                        {/* <p className="text-lg">Rp 15</p> */}
-                        <p className="text-lg font-bold text-indigo-700">Rp 242.75</p>
+                        <p className="text-lg">Rp {totals.subtotal.toFixed(2)}</p>
+                        <p className="text-lg">Rp {totals.tax.toFixed(2)}</p>
+                        <p className="text-lg font-bold text-indigo-700">Rp {totals.total.toFixed(2)}</p>
                     </div>
                 </div>
                 <div className="flex justify-between items-center mt-4">
                     <button
                         type="button"
-                        onClick={toggleEditing}
+                        onClick={() => navigate(-1)}
                         className="py-2 px-4 me-2 w-1/2 bg-black text-white rounded-full hover:bg-gray-700 transition duration-200"
                     >
-                        {isEditing ? 'Save' : 'Edit'}
+                        Edit
                     </button>
                     <button
                         type="submit"
@@ -242,7 +172,8 @@ const ReceiptList = () => {
                     </button>
                 </div>
             </form>
-        </div>
+            {/* Dummy */}
+        </div >
     );
 };
 
